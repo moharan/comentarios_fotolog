a2dad4eace3934419ab7d73e9a9d1565ee63a59e
RangiBecerra <rangibecerra@gmail.com>
2017-06-18 01:32:39 -0400

    botones funcionando

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
-/*aquí va tu código*/
-var nombre = document.getElementById("nombre").value;
-var comentario = document.getElementById("comentario").value;
+//alert("existe");
+function guardar() {
+	var button = document.getElementById("guardar");
+	button.addEventListener("click", guardarDatos);
+	guardarDatos();
+};
 
-function agregarComentario(){
+function guardarDatos() {
+	var nombre = document.getElementById("nombre").value;
+	var comentario = document.getElementById("comentario").value;
+	// recorre los datos de el formulario
+	sessionStorage.setItem(nombre, comentario);
 
-	if((localStorage.nombre != undefined) && (localStorage.comentario != undefined)) {
-		console.log(localStorage.nombre);
-		console.log(localStorage.comentario);
-		document.getElementById("ale").innerHTML= "Nombre: " + localStorage.nombre + "<br/> Comentario: " + localStorage.comentario + "<br>";
-	}	
-	else {
-		document.getElementById("aviso").innerHTML= "working";
+	var contenedor = document.getElementById("ale");
+	contenedor.innerHTML = " ";
+	for (var i = 0; i < sessionStorage.length; i++) {
+		// devuelve el (numero) de tu key (Application)
+		var kei = sessionStorage.key(i);
+		var valuee = sessionStorage.getItem(kei);
+		// = si solo escribo =, se sobreescribe USAR (+=)
+		contenedor.innerHTML += "<p>" + kei + " " + valuee + "</p>";
 	}
+};
+
+function limpiar() {
+	var button = document.getElementById("limpiar");
+	button.addEventListener("click", limpiarDatos);
+	limpiarDatos();
+};
+
+function limpiarDatos() {
+	alert("En algun momento borrara los datos");
 }
-agregarComentario();
+
+guardar();
+limpiar();
