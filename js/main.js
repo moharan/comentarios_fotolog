e473bd4741bbf0125424855638d2d55d851d9c76
RangiBecerra <rangibecerra@gmail.com>
2017-06-18 01:04:41 -0400

    cambio imagenes y estilo de contenido

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 /*aquí va tu código*/
+var nombre = document.getElementById("nombre").value;
+var comentario = document.getElementById("comentario").value;
+
+function agregarComentario(){
+
+	if((localStorage.nombre != undefined) && (localStorage.comentario != undefined)) {
+		console.log(localStorage.nombre);
+		console.log(localStorage.comentario);
+		document.getElementById("ale").innerHTML= "Nombre: " + localStorage.nombre + "<br/> Comentario: " + localStorage.comentario + "<br>";
+	}	
+	else {
+		document.getElementById("aviso").innerHTML= "working";
+	}
+}
+agregarComentario();
